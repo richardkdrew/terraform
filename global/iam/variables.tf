ea4579ce225ef1f7c98b0cc91d43446dadcfd7b8
Richard Drew <richardkdrew@users.noreply.github.com>
2017-11-13 16:17:01 +1300

    Added missing variable

--- global/iam/variables.tf
+++ global/iam/variables.tf
@@ -1,3 +1,7 @@
+variable "bucket_deployer" {
+	description = "The name of the user to create (as a deployment user for the bucket), e.g. bucket-deployer"
+}
+
 variable bucket_name {
 	description = "The name of the S3 bucket to configure the deployemnt user for, e.g. your-bucket-name"
 }
